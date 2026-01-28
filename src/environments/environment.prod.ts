23953baaf89b34725b0a12337407b71a0b010f9d
IvanRamirezFrancisco <ivanfra12345@gmail.com>
2026-01-28 16:56:39 -0600

    Conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-te8q.onrender.com',
+  apiUrl: 'https://backend-te8q.onrender.com/api',
   appName: 'Casa de Música Castillo',
   version: '2.0.0'
 };
